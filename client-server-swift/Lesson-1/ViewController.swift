af94eedfe5b79f7097aa9873e2aa5b469e374d97
Алексей Пеньков <alexey_penkov@list.ru>
2021-05-24 09:25:02 +0800

    Init project

---
+++ client-server-swift/Lesson-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server-swift
+//
+//  Created by Алексей Пеньков on 24.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
